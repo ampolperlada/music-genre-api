6a261110317421d72a8d53eed2664f1bd260c115
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-02-07 16:01:37 +0800

    installed cors

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const genreRoutes = require("./routes/genres");
+app.use("/api/genres", genreRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
